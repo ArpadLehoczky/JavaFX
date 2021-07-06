713dd0ccd64a476b707676854c8fb3bf91346d35
Lehoczky Árpád <86922205+ArpadLehoczky@users.noreply.github.com>
2021-07-06 09:14:05 +0200

    Prototype
    Működő JavaFX alapú prototípus

---
+++ src/phonebook/DB.java
@@ -0,0 +1,105 @@
+package phonebook;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ * @author Leho
+ */
+public class DB {
+
+    final String URL = "jdbc:derby:sampleDB;create=true";
+    final String USERNAME = "";
+    final String PASSWORD = "";
+
+    //Létrehozzuk a kapcsolatot
+    Connection conn = null;
+    Statement createStatement = null;
+    DatabaseMetaData dbmd = null;
+
+    public DB() {
+        //Megpróbáljuk életre kelteni
+        try {
+            conn = DriverManager.getConnection(URL);
+            System.out.println("A híd létrejött");
+        } catch (SQLException ex) {
+            System.out.println("Valami baj van a connection (híd) létrehozásakor: " + ex);
+        }
+
+        //Ha életre kelt, akkor csinálunk egy megpakolható teherautót
+        if (conn != null) {
+            try {
+                createStatement = conn.createStatement();
+            } catch (SQLException ex) {
+                System.out.println("Valami baj van a createStatement (teherautó) létrehozásakor: " + ex);
+            }
+        }
+
+        try {
+            //Megnézzük, hogy üres-e az adatbázis. Létezik-e az adattábla?
+            dbmd = conn.getMetaData();
+        } catch (SQLException ex) {
+            System.out.println("Valami baj van a DatabaseMetaData (adatbázis leírása) létrehozásakor: " + ex);
+        }
+
+        try {
+            ResultSet rs1 = dbmd.getTables(null, "APP", "CONTACTS", null);
+            if (!rs1.next()) {
+                createStatement.execute("create table contacts(id INT not null primary key GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), lastname varchar(20), firstname varchar(20), email varchar(30))");
+            }
+        } catch (SQLException ex) {
+            System.out.println("Valami baj van az adattáblák létrehozásakor: " + ex);;
+        }
+    }
+
+    public ArrayList<Person> getAllContacts() {
+        String sql = "select * from contacts";
+        ArrayList<Person> users = null;
+        try {
+            ResultSet rs = createStatement.executeQuery(sql);
+            users = new ArrayList<>();
+
+            while (rs.next()) {
+                Person actualPerson = new Person(rs.getInt("id"), rs.getString("lastname"), rs.getString("firstname"), rs.getString("email"));
+                users.add(actualPerson);
+            }
+        } catch (SQLException ex) {
+            System.out.println("Valami baj van a userek kiolvasásakor: " + ex);
+        }
+        return users;
+
+    }
+    
+    public void addContact(Person person){
+        try {
+           String sql = "insert into contacts (lastname, firstname, email) values (?,?,?)";
+           PreparedStatement preparedStatement = conn.prepareStatement(sql);
+           preparedStatement.setString(1, person.getLastName());
+           preparedStatement.setString(2, person.getFirstName());
+           preparedStatement.setString(3, person.getEmail());
+           preparedStatement.execute();
+        } catch (SQLException ex) {
+            System.out.println("Valami baj van a contact hozzáadásakor: " + ex);
+        }
+    }
+    
+    public void updateContact(Person person){
+        try {
+           String sql = "update contacts set lastname = ?, firstname = ?, email = ? where id = ?";
+           PreparedStatement preparedStatement = conn.prepareStatement(sql);
+           preparedStatement.setString(1, person.getLastName());
+           preparedStatement.setString(2, person.getFirstName());
+           preparedStatement.setString(3, person.getEmail());
+           preparedStatement.setInt(4, Integer.parseInt(person.getId()));
+           preparedStatement.execute();
+        } catch (SQLException ex) {
+            System.out.println("Valami baj van a contact hozzáadásakor: " + ex);
+        }
+    }
+}
